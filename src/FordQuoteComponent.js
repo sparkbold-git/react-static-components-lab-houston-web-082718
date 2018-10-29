169aea388145ebf8df5b68598ae4ce83d5818710
sparkbold-git <sparkboldstudio@gmail.com>
2018-10-29 10:37:19 -0500

    Done.

--- src/FordQuoteComponent.js
+++ src/FordQuoteComponent.js
@@ -1,10 +1,15 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 export default class FordQuoteComponent extends Component {
   render() {
     return (
       <div className="bar" id="ford">
-        <i><p>&quot;If I had asked people what they wanted, they would have said faster horses&quot;</p></i>
+        <i>
+          <p>
+            &quot;If I had asked people what they wanted, they would have said
+            faster horses&quot;
+          </p>
+        </i>
         <br />
         &#9;-Henry Ford
       </div>
